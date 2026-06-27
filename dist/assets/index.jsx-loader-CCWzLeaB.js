684c48bbfb56ec700c33b65eea2d97f1f38fb7aa
taaeee <osvaldo106@hotmail.com>
2026-06-26 20:09:52 -0600

    feat: add queue monitor and UI search overlay components

--- dist/assets/index.jsx-loader-CCWzLeaB.js
+++ dist/assets/index.jsx-loader-CCWzLeaB.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.jsx-XYcMN1XH.js")
+      chrome.runtime.getURL("assets/index.jsx-D1XHFhAM.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
